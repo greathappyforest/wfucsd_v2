aafd6b04253574835e78bb0cae60afece6140ae9
lyl1988 <122391887@qq.com>
2017-07-08 02:26:30 -0700

    combine f&b

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,22 +10,22 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     routes: [{
-            path: 'http://172.104.103.251:9000/',
+            path: '/',
             name: 'wfucsd',
             component: Home
         },
         {
-            path: 'http://172.104.103.251:9000/members',
+            path: '/members',
             name: 'members',
             component: Members
         },
         {
-            path: 'http://172.104.103.251:9000/lottery',
+            path: '/lottery',
             name: 'wfucsd lottery',
             component: Lottery
         },
         {
-            path: 'http://172.104.103.251:9000/Marketplace',
+            path: '/Marketplace',
             name: 'wfucsd marketplace',
             component: Marketplace
         }
